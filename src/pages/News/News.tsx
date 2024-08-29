bd5d892c7750ef1248e33c8c0feddc3b0183d282
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-08-29 11:08:48 +0300

    Add news get request to the news page

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -1,7 +1,39 @@
 import { Typography } from '@mui/material';
+import { AnimeClient, JikanResponse, JikanNews } from '@tutkli/jikan-ts';
+import { useEffect, useState } from 'react';
 
 function News() {
-	return <Typography variant="h2">Welcome to the News Page</Typography>;
+	const animeClient = new AnimeClient();
+	const [newsList, setNewsList] = useState<JikanNews[]>([]);
+
+	useEffect(() => {
+		const fetchNews = async () => {
+			try {
+				const response: JikanResponse<JikanNews[]> =
+					await animeClient.getAnimeNews(1, 1);
+				console.log(response, 'resp');
+				console.log(response.data, 'res');
+				setNewsList(response.data);
+			} catch (error) {
+				console.error('Failed to fetch news:', error);
+			}
+		};
+
+		if (newsList.length === 0) {
+			fetchNews();
+		}
+	}, [animeClient, newsList]);
+
+	return (
+		<div>
+			<Typography variant="h2">Anime News</Typography>
+			{newsList.map((news, index) => (
+				<Typography key={index} variant="body1">
+					{news.title}
+				</Typography>
+			))}
+		</div>
+	);
 }
 
 export default News;
