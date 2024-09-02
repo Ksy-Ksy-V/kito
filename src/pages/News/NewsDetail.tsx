7aec0b5e24f14f48920e46872d673cd4c4bb9d77
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-09-02 14:33:07 +0300

    update detail news page

--- src/pages/News/NewsDetail.tsx
+++ src/pages/News/NewsDetail.tsx
@@ -3,98 +3,99 @@ import newsImg from '../../images/animeCardPoster.png';
 
 const NewsDetail = () => {
 	return (
-		<Grid2 container spacing={2}>
+		<Box
+			sx={{
+				position: 'relative',
+				width: '100vw',
+				left: '50%',
+				right: '50%',
+				marginLeft: '-50vw',
+				marginRight: '-50vw',
+			}}
+		>
 			<Grid2
-				size={{ xs: 12 }}
-				sx={{
-					marginTop: '1rem',
+				container
+				spacing={2}
+				sx={{ width: '100%', maxWidth: '100%' }}
+			>
+				<Grid2
+					size={{ xs: 12 }}
+					sx={{
+						position: 'relative',
+						height: '400px',
+						width: '100%',
+						backgroundImage: `url(${newsImg})`,
+						backgroundSize: 'cover',
+						backgroundPosition: 'center',
+						marginTop: '1rem',
+						boxShadow:
+							'0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.2)',
+						'&::before': {
+							content: '""',
+							position: 'absolute',
+							top: 0,
+							left: 0,
+							right: 0,
+							bottom: 0,
+							backgroundColor: 'rgba(0, 0, 0, 0.7)',
+							zIndex: 1,
+						},
+					}}
+				></Grid2>
 
-					position: 'relative',
-					height: '400px',
-					width: '100%',
-					backgroundImage: `url(${newsImg})`,
-					backgroundSize: 'cover',
-					backgroundPosition: 'center',
-					boxShadow:
-						'0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.2)',
-					'&::before': {
-						content: '""',
+				<Grid2
+					container
+					spacing={2}
+					sx={{
+						width: '100%',
+						maxWidth: '100%',
 						position: 'absolute',
 						top: 0,
 						left: 0,
-						right: 0,
-						bottom: 0,
-						backgroundColor: 'rgba(0, 0, 0, 0.7)',
-						zIndex: 1,
-					},
-				}}
-			>
-				<Grid2
-					size={{ xs: 6 }}
-					sx={{
-						position: 'relative',
+						height: '400px',
+						alignItems: 'center',
+						justifyContent: 'center',
 						zIndex: 2,
-						padding: '2rem',
-						textAlign: 'center',
-						// display: 'center',
 					}}
 				>
-					<Typography variant="h2">News Title</Typography>
-					<Typography variant="h4">News Author</Typography>
-					{/* 
-					<Grid2 size={{ xs: 6 }} offset={{ xs: 3 }}> */}
-					<Box
+					<Grid2
+						size={{ xs: 6 }}
 						sx={{
 							position: 'relative',
-
-							// width: '50%',
-							marginTop: '3rem',
-							zIndex: 3,
+							zIndex: 2,
 							padding: '2rem',
 							textAlign: 'center',
-							background: 'rgba(29, 51, 53, 0.7)',
-							borderRadius: '5px',
-							boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
-							backdropFilter: 'blur(4.9px)',
-							webkitBackdropFilter: 'blur(4.9px)',
-							border: '1px solid rgba(29, 51, 53, 0.3)',
 						}}
 					>
-						<Typography variant="body1">
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit, sed do eiusmod tempor incididunt ut labore et
-							dolore magna aliqua. Ut enim ad minim veniam, quis
-							nostrud exercitation ullamco laboris nisi ut aliquip
-							ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore
-							eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia
-							deserunt mollit anim id est laborum. Lorem ipsum
-							dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna
-							aliqua. Ut enim ad minim veniam, quis nostrud
-							exercitation ullamco laboris nisi ut aliquip ex ea
-							commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore
-							eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia
-							deserunt mollit anim id est laborum. Lorem ipsum
-							dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna
-							aliqua. Ut enim ad minim veniam, quis nostrud
-							exercitation ullamco laboris nisi ut aliquip ex ea
-							commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore
-							eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia
-							deserunt mollit anim id est laborum.
-						</Typography>
-						<Typography variant="body2">News Date</Typography>
-					</Box>
-					{/* </Grid2> */}
+						<Typography variant="h2">News Title</Typography>
+						<Typography variant="h4">News Author</Typography>
+
+						<Box
+							sx={{
+								position: 'relative',
+								marginTop: '3rem',
+								zIndex: 3,
+								padding: '2rem',
+								textAlign: 'center',
+								background: 'rgba(29, 51, 53, 0.7)',
+								borderRadius: '5px',
+								boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+								backdropFilter: 'blur(4.9px)',
+								webkitBackdropFilter: 'blur(4.9px)',
+								border: '1px solid rgba(29, 51, 53, 0.3)',
+							}}
+						>
+							<Typography variant="body1">
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua...
+							</Typography>
+							<Typography variant="body2">News Date</Typography>
+						</Box>
+					</Grid2>
 				</Grid2>
 			</Grid2>
-		</Grid2>
+		</Box>
 	);
 };
 
