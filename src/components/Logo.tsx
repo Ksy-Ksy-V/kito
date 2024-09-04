31d40edf582a8b9ecd33f0a5d2328eadf77a8455
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-09-04 10:41:49 +0300

    Update logo; Add favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
-import logo from '../images/logo.png';
+import logo from '../images/logoKito.png';
 
 const Logo: React.FC = () => {
 	return (
