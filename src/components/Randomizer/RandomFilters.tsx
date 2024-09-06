17fa3e1da58c2b00c03615b7b748a625b84a0eeb
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-09-06 13:16:11 +0300

    Add skeleton to Randomizer page

--- src/components/Randomizer/RandomFilters.tsx
+++ src/components/Randomizer/RandomFilters.tsx
@@ -5,6 +5,7 @@ import {
 	FormControl,
 	Autocomplete,
 	TextField,
+	Skeleton,
 } from '@mui/material';
 import {
 	GenresClient,
@@ -22,6 +23,7 @@ import ClearIcon from '@mui/icons-material/Clear';
 const RandomFilters = () => {
 	const [animeGenres, setAnimeGenres] = useState<Genre[]>([]);
 	const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+	const [loading, setLoading] = useState(true);
 	const animeTypes: AnimeType[] = [
 		'TV',
 		'Movie',
@@ -36,14 +38,12 @@ const RandomFilters = () => {
 		'upcoming',
 	];
 	const animeRatings: AnimeRating[] = ['g', 'pg', 'pg13', 'r17', 'r'];
-
 	const [selectedType, setSelectedType] = useState<AnimeType | ''>('');
 	const [selectedStatus, setSelectedStatus] = useState<
 		AnimeSearchStatus | ''
 	>('');
 	const [selectedRating, setSelectedRating] = useState<AnimeRating | ''>('');
 	const navigate = useNavigate();
-
 	const theme = useTheme();
 
 	useEffect(() => {
@@ -52,10 +52,11 @@ const RandomFilters = () => {
 				const genresClient = new GenresClient();
 				const response: JikanResponse<Genre[]> =
 					await genresClient.getAnimeGenres();
-
 				setAnimeGenres(response.data);
 			} catch (error) {
 				console.error('Failed to fetch anime genres:', error);
+			} finally {
+				setLoading(false);
 			}
 		};
 		if (!animeGenres || animeGenres.length === 0) {
@@ -76,18 +77,15 @@ const RandomFilters = () => {
 		if (selectedGenre) {
 			queryParams.push(`genre=${selectedGenre.mal_id}`);
 		}
-
 		if (selectedType) {
 			queryParams.push(`type=${selectedType}`);
 		}
-
 		if (selectedStatus) {
 			queryParams.push(`status=${selectedStatus}`);
 		}
 		if (selectedRating) {
 			queryParams.push(`rating=${selectedRating}`);
 		}
-
 		const queryString =
 			queryParams.length > 0 ? `?${queryParams.join('&')}` : '';
 		navigate(`/randomizer-search${queryString}`);
@@ -96,46 +94,50 @@ const RandomFilters = () => {
 	return (
 		<Grid2 container spacing={2}>
 			<Grid2 size={{ xs: 6 }} offset={{ xs: 3 }}>
-				<FormControl fullWidth variant="filled">
-					<Autocomplete
-						options={animeGenres}
-						getOptionLabel={(option) => option.name}
-						value={selectedGenre}
-						clearIcon={
-							<ClearIcon
-								sx={[{ visibility: 'visible' }]}
-								fontSize="small"
-							/>
-						}
-						onChange={handleGenreChange}
-						renderOption={(props, option) => {
-							const { key, ...rest } = props;
-							return (
-								<li
-									key={key}
-									{...rest}
-									onMouseEnter={(e) =>
-										(e.currentTarget.style.backgroundColor =
-											theme.palette.primary.main)
-									}
-									onMouseLeave={(e) =>
-										(e.currentTarget.style.backgroundColor =
-											'inherit')
-									}
-								>
-									{option.name}
-								</li>
-							);
-						}}
-						renderInput={(params) => (
-							<TextField
-								{...params}
-								variant="filled"
-								label="Genre"
-							/>
-						)}
-					/>
-				</FormControl>
+				{loading ? (
+					<Skeleton variant="rectangular" width="100%" height={56} />
+				) : (
+					<FormControl fullWidth variant="filled">
+						<Autocomplete
+							options={animeGenres}
+							getOptionLabel={(option) => option.name}
+							value={selectedGenre}
+							clearIcon={
+								<ClearIcon
+									sx={[{ visibility: 'visible' }]}
+									fontSize="small"
+								/>
+							}
+							onChange={handleGenreChange}
+							renderOption={(props, option) => {
+								const { key, ...rest } = props;
+								return (
+									<li
+										key={key}
+										{...rest}
+										onMouseEnter={(e) =>
+											(e.currentTarget.style.backgroundColor =
+												theme.palette.primary.main)
+										}
+										onMouseLeave={(e) =>
+											(e.currentTarget.style.backgroundColor =
+												'inherit')
+										}
+									>
+										{option.name}
+									</li>
+								);
+							}}
+							renderInput={(params) => (
+								<TextField
+									{...params}
+									variant="filled"
+									label="Genre"
+								/>
+							)}
+						/>
+					</FormControl>
+				)}
 
 				<SelectForm
 					label="Type"
@@ -171,12 +173,21 @@ const RandomFilters = () => {
 					upperCaseOptions
 				/>
 
-				<StyledButton
-					onClick={handleRandomize}
-					sx={{ marginTop: '3rem', marginBottom: '2rem' }}
-				>
-					Randomize
-				</StyledButton>
+				{loading ? (
+					<Skeleton
+						variant="rectangular"
+						width="100%"
+						height={40}
+						sx={{ marginTop: '3rem', marginBottom: '2rem' }}
+					/>
+				) : (
+					<StyledButton
+						onClick={handleRandomize}
+						sx={{ marginTop: '3rem', marginBottom: '2rem' }}
+					>
+						Randomize
+					</StyledButton>
+				)}
 			</Grid2>
 		</Grid2>
 	);
