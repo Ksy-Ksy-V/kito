4d893b0bd43f6f1a2533f86db9858e4843c9715c
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-09-09 22:29:59 +0300

    Wip: search filter

--- src/components/Search/SearchFilter.tsx
+++ src/components/Search/SearchFilter.tsx
@@ -1,7 +1,41 @@
-import { TextField, MenuItem, Box, IconButton, Grid } from '@mui/material';
+import { useState, useEffect } from 'react';
+import {
+	TextField,
+	MenuItem,
+	IconButton,
+	Grid2,
+	FormControl,
+	Autocomplete,
+} from '@mui/material';
 import TuneOutlinedIcon from '@mui/icons-material/TuneOutlined';
+import { Anime, AnimeClient, JikanResponse } from '@tutkli/jikan-ts';
 
 const SearchFilter = () => {
+	const [searchTerm, setSearchTerm] = useState('');
+	const [animeOptions, setAnimeOptions] = useState<Anime[]>([]);
+	const [animeList, setAnimeList] = useState<Anime[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<string | null>(null);
+
+	const handleSearch = async (query: string) => {
+		setLoading(true);
+		setError(null);
+		try {
+			const animeClient = new AnimeClient();
+			const response: JikanResponse<Anime[]> =
+				await animeClient.getAnimeSearch({
+					q: query,
+					limit: 10,
+				});
+			setAnimeOptions(response.data);
+			setAnimeList(response.data);
+			setLoading(false);
+		} catch (err) {
+			console.error('Failed to search anime:', err);
+			setError('Something went wrong during the search.');
+		}
+	};
+
 	const options = [
 		{ value: 'option1', label: 'Option 1' },
 		{ value: 'option2', label: 'Option 2' },
@@ -10,120 +44,115 @@ const SearchFilter = () => {
 	];
 
 	return (
-		<Grid container spacing={2}>
-			<Grid item xs={12}>
-				<Box
-					sx={{
-						display: 'flex',
-						alignItems: 'center',
-						marginBottom: '2rem',
-					}}
+		<Grid2 container spacing={2} size={12}>
+			<Grid2 size={3}>
+				<FormControl fullWidth variant="filled">
+					<Autocomplete
+						options={animeOptions}
+						getOptionLabel={(option) => option.title}
+						value={null}
+						onInputChange={(_, newInputValue) => {
+							setSearchTerm(newInputValue);
+							if (newInputValue) {
+								handleSearch(newInputValue);
+							}
+						}}
+						loading={loading}
+						noOptionsText={
+							searchTerm
+								? 'No results'
+								: 'What anime are you looking for?'
+						}
+						renderInput={(params) => (
+							<TextField
+								{...params}
+								label="Search for Anime"
+								variant="outlined"
+								fullWidth
+							/>
+						)}
+					/>
+				</FormControl>
+			</Grid2>
+
+			<Grid2 size={2}>
+				<TextField
+					id="outlined-select-1"
+					select
+					label="Filter 1"
+					defaultValue={options[0].value}
+					variant="outlined"
 				>
-					<Grid item xs={3}>
-						<TextField
-							id="outlined-basic"
-							label="Search"
-							variant="outlined"
-							sx={{ minWidth: '200px' }}
-						/>
-					</Grid>
+					{options.map((option) => (
+						<MenuItem key={option.value} value={option.value}>
+							{option.label}
+						</MenuItem>
+					))}
+				</TextField>
+			</Grid2>
 
-					<Grid item xs={2}>
-						<TextField
-							id="outlined-select-1"
-							select
-							label="Filter 1"
-							defaultValue={options[0].value}
-							variant="outlined"
-							sx={{ minWidth: '150px' }}
-						>
-							{options.map((option) => (
-								<MenuItem
-									key={option.value}
-									value={option.value}
-								>
-									{option.label}
-								</MenuItem>
-							))}
-						</TextField>
-					</Grid>
+			<Grid2 size={2}>
+				<TextField
+					id="outlined-select-2"
+					select
+					label="Filter 2"
+					defaultValue={options[0].value}
+					variant="outlined"
+				>
+					{options.map((option) => (
+						<MenuItem key={option.value} value={option.value}>
+							{option.label}
+						</MenuItem>
+					))}
+				</TextField>
+			</Grid2>
 
-					<Grid item xs={2}>
-						<TextField
-							id="outlined-select-2"
-							select
-							label="Filter 2"
-							defaultValue={options[0].value}
-							variant="outlined"
-							sx={{ minWidth: '150px' }}
-						>
-							{options.map((option) => (
-								<MenuItem
-									key={option.value}
-									value={option.value}
-								>
-									{option.label}
-								</MenuItem>
-							))}
-						</TextField>
-					</Grid>
+			<Grid2 size={2}>
+				<TextField
+					id="outlined-select-3"
+					select
+					label="Filter 3"
+					defaultValue={options[0].value}
+					variant="outlined"
+				>
+					{options.map((option) => (
+						<MenuItem key={option.value} value={option.value}>
+							{option.label}
+						</MenuItem>
+					))}
+				</TextField>
+			</Grid2>
+			<Grid2 size={2}>
+				<TextField
+					id="outlined-select-4"
+					select
+					label="Filter 4"
+					defaultValue={options[0].value}
+					variant="outlined"
+				>
+					{options.map((option) => (
+						<MenuItem key={option.value} value={option.value}>
+							{option.label}
+						</MenuItem>
+					))}
+				</TextField>
+			</Grid2>
 
-					<Grid item xs={2}>
-						<TextField
-							id="outlined-select-3"
-							select
-							label="Filter 3"
-							defaultValue={options[0].value}
-							variant="outlined"
-							sx={{ minWidth: '150px' }}
-						>
-							{options.map((option) => (
-								<MenuItem
-									key={option.value}
-									value={option.value}
-								>
-									{option.label}
-								</MenuItem>
-							))}
-						</TextField>
-					</Grid>
-					<Grid item xs={2}>
-						<TextField
-							id="outlined-select-4"
-							select
-							label="Filter 4"
-							defaultValue={options[0].value}
-							variant="outlined"
-							sx={{ minWidth: '150px' }}
-						>
-							{options.map((option) => (
-								<MenuItem
-									key={option.value}
-									value={option.value}
-								>
-									{option.label}
-								</MenuItem>
-							))}
-						</TextField>
-					</Grid>
-
-					<Grid item xs={1}>
-						<IconButton
-							color="inherit"
-							// onClick={handleMenuClick}
-							// sx={{
-							// 	color: theme.palette.primary.main,
-							// 	'&:hover': {
-							// 		color: theme.palette.secondary.main,
-							// 	},
-							// }}
-						>
-							<TuneOutlinedIcon sx={{ fontSize: '1.5rem' }} />
-						</IconButton>
-					</Grid>
-				</Box>
-			</Grid>
-		</Grid>
+			<Grid2 size={1}>
+				<IconButton
+					color="inherit"
+					// onClick={handleMenuClick}
+					// sx={{
+					// 	color: theme.palette.primary.main,
+					// 	'&:hover': {
+					// 		color: theme.palette.secondary.main,
+					// 	},
+					// }}
+				>
+					<TuneOutlinedIcon sx={{ fontSize: '1.5rem' }} />
+				</IconButton>
+			</Grid2>
+		</Grid2>
 	);
 };
 
--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -61,96 +61,26 @@
 
 // export default Search;
 
-import React, { useState } from 'react';
-import { TextField, Grid, Typography, Autocomplete } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Grid2, Typography } from '@mui/material';
 import { AnimeClient, JikanResponse, Anime } from '@tutkli/jikan-ts';
 
-import SearchCard from '../../components/Search/SearchCard';
+import SearchFilter from '../../components/Search/SearchFilter';
 
-const SearchWithAutocomplete: React.FC = () => {
-	const [searchTerm, setSearchTerm] = useState('');
-	const [animeOptions, setAnimeOptions] = useState<Anime[]>([]);
-	const [animeList, setAnimeList] = useState<Anime[]>([]);
-	const [selectedAnime, setSelectedAnime] = useState<Anime | null>(null);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState<string | null>(null);
-
-	const handleSearch = async (query: string) => {
-		setLoading(true);
-		setError(null);
-		try {
-			const animeClient = new AnimeClient();
-			const response: JikanResponse<Anime[]> =
-				await animeClient.getAnimeSearch({
-					q: query,
-					limit: 10,
-				});
-			setAnimeOptions(response.data);
-			setAnimeList(response.data);
-			setLoading(false);
-		} catch (err) {
-			console.error('Failed to search anime:', err);
-			setError('Something went wrong during the search.');
-		}
-	};
-
+const Search: React.FC = () => {
 	return (
-		<div>
-			<Autocomplete
-				options={animeOptions}
-				getOptionLabel={(option) => option.title}
-				value={null}
-				onInputChange={(_, newInputValue) => {
-					setSearchTerm(newInputValue);
-					if (newInputValue) {
-						handleSearch(newInputValue);
-					}
-				}}
-				loading={loading}
-				noOptionsText="No results"
-				renderInput={(params) => (
-					<TextField
-						{...params}
-						label="Search for Anime"
-						variant="outlined"
-						fullWidth
-					/>
-				)}
-			/>
-
-			<Grid container spacing={2} sx={{ marginTop: '2rem' }}>
-				{loading ? (
-					<Typography variant="body1">Loading...</Typography>
-				) : error ? (
-					<Typography variant="body1" color="error">
-						{error}
-					</Typography>
-				) : animeList.length === 0 && searchTerm ? (
-					<Typography variant="body1">No results found.</Typography>
-				) : (
-					animeList.map((anime) => (
-						<Grid
-							item
-							xs={12}
-							sm={6}
-							md={4}
-							lg={3}
-							key={anime.mal_id}
-						>
-							<SearchCard
-								title={anime.title}
-								description={
-									anime.synopsis ||
-									'No description available.'
-								}
-								imageUrl={anime.images.jpg.image_url || ''}
-							/>
-						</Grid>
-					))
-				)}
-			</Grid>
-		</div>
+		<Grid2 container spacing={2}>
+			<Grid2 size={{ xs: 12 }}>
+				<Typography
+					variant="h1"
+					sx={{ textAlign: 'center', margin: '1rem' }}
+				>
+					There's something for everyone!
+				</Typography>
+				<SearchFilter />
+			</Grid2>
+		</Grid2>
 	);
 };
 
-export default SearchWithAutocomplete;
+export default Search;
