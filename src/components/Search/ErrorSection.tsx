0b7ddd95e871d666e8e2d58ad23bde3c86cc2602
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-10-16 14:10:41 +0300

    Create components for main section

--- src/components/Search/ErrorSection.tsx
+++ src/components/Search/ErrorSection.tsx
@@ -1,5 +1,69 @@
+import { Grid2, Typography } from '@mui/material';
+import theme from '../../styles/theme';
+import errorImg from '../../images/errorImg.png';
+import StyledButton from '../StyledButton';
+
 const ErrorSection = () => {
-	return <div>ErrorSection</div>;
+	const handleReload = () => {
+		window.location.reload();
+	};
+	return (
+		<Grid2 size={12}>
+			<Typography
+				variant="h3"
+				sx={{
+					zIndex: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+					display: 'flex',
+					flexDirection: 'column',
+					marginTop: '1rem',
+					fontSize: {
+						xs: theme.typography.h5.fontSize,
+						sm: theme.typography.h4.fontSize,
+						md: theme.typography.h4.fontSize,
+						lg: theme.typography.h3.fontSize,
+						xl: theme.typography.h3.fontSize,
+					},
+				}}
+			>
+				Sorry... Something went wrong
+			</Typography>
+
+			<Grid2
+				sx={{
+					width: '100%',
+					height: '400px',
+					backgroundImage: `url(${errorImg})`,
+					backgroundSize: 'contain',
+					backgroundPosition: 'center',
+					backgroundRepeat: 'no-repeat',
+					zIndex: 0,
+				}}
+			></Grid2>
+
+			<Grid2
+				sx={{
+					marginTop: '1rem',
+					zIndex: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+			>
+				<StyledButton
+					sx={{
+						width: { xl: '20rem', sm: '15rem' },
+					}}
+					onClick={handleReload}
+				>
+					{' '}
+					Try again
+				</StyledButton>
+			</Grid2>
+		</Grid2>
+	);
 };
 
 export default ErrorSection;
