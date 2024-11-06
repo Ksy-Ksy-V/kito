ef8ec5cd68a24de9d4dbe8dbfabac1becf4b0e26
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-11-06 14:25:06 +0200

    Optimization footer for large screen and mobile

--- src/components/Header/MenuSmall.tsx
+++ src/components/Header/MenuSmall.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Divider, IconButton, Menu, MenuItem } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { menuStyles, menuItemStyles } from '../styles/menuStyles';
+import { menuStyles, menuItemStyles } from '../../styles/menuStyles';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import theme from '../styles/theme';
+import theme from '../../styles/theme';
 
 const MenuSmall = () => {
 	const [anchorElBrowse, setAnchorElBrowse] = useState<null | HTMLElement>(
