c3166b34888a94ca4685a89d9421679506d5daea
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-11-25 13:15:20 +0200

    Fixed vercel deploy

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../store/hooks';
 import { selectAuth } from '../store/reducers/authSlice';
-import { history } from '../models/history';
+import { history } from '../models/History';
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
 	const { isLoggedIn } = useAppSelector(selectAuth);
