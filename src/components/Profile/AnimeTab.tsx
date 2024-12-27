a56b9c3b12b77b48384b500e258fdd2ed69665fc
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2024-12-27 19:55:19 +0200

    Added filters for tabs

--- src/components/Profile/AnimeTab.tsx
+++ src/components/Profile/AnimeTab.tsx
@@ -24,6 +24,16 @@ interface Tab {
 	value: string;
 }
 
+interface RatingOption {
+	label: string;
+	value: string;
+}
+
+interface TypeOption {
+	label: string;
+	value: string;
+}
+
 const AnimeTabs = () => {
 	const [page, setPage] = useState(1);
 	const [itemsPerPage] = useState<number>(18);
@@ -38,8 +48,34 @@ const AnimeTabs = () => {
 		{ label: 'Score', value: 'Score' },
 	];
 
+	const ratingOptions: RatingOption[] = [
+		{ label: 'All', value: 'All' },
+		{ label: 'This is Legendary - 10!!!', value: '10' },
+		{ label: 'Almost Perfect - 9', value: '9' },
+		{ label: 'Impressive - 8', value: '8' },
+		{ label: 'Pretty Good - 7', value: '7' },
+		{ label: 'Decent but Flawed - 6', value: '6' },
+		{ label: 'Just OK - 5', value: '5' },
+		{ label: 'Mediocre at Best - 4', value: '4' },
+		{ label: 'Needs Improvement - 3', value: '3' },
+		{ label: 'Barely Watchable - 2', value: '2' },
+		{ label: 'Complete Disaster - 1', value: '1' },
+	];
+
+	const typeOptions: TypeOption[] = [
+		{ label: 'All', value: 'All' },
+		{ label: 'TV', value: 'TV' },
+		{ label: 'Movie', value: 'Movie' },
+		{ label: 'OVA', value: 'OVA' },
+		{ label: 'Special', value: 'Special' },
+		{ label: 'ONA', value: 'ONA' },
+	];
+
 	const [activeTab, setActiveTab] = useState<string>(tabs[0].value);
 	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+	const [ratingFilter, setRatingFilter] = useState<string>('All');
+	const [typeFilter, setTypeFilter] = useState<string>('All');
 
 	const getAnimeCount = (tabValue: string) => {
 		if (tabValue === 'Score') {
@@ -74,11 +110,32 @@ const AnimeTabs = () => {
 		setPage(value);
 	};
 
-	const filteredAnime = user.animeList.filter((anime) => {
-		if (activeTab === 'Score') {
-			return anime.userRating !== undefined;
+	const handleFilterChange = (
+		event: SelectChangeEvent<string>,
+		filterType: 'rating' | 'type'
+	) => {
+		if (filterType === 'rating') {
+			setRatingFilter(event.target.value);
+		} else if (filterType === 'type') {
+			setTypeFilter(event.target.value);
 		}
-		return anime.listName === activeTab;
+		setPage(1);
+	};
+
+	const filteredAnime = user.animeList.filter((anime) => {
+		if (activeTab === 'Score' && anime.userRating === undefined)
+			return false;
+		if (activeTab !== 'Score' && anime.listName !== activeTab) return false;
+
+		if (
+			ratingFilter !== 'All' &&
+			anime.userRating?.toString() !== ratingFilter
+		)
+			return false;
+
+		if (typeFilter !== 'All' && anime.type !== typeFilter) return false;
+
+		return true;
 	});
 
 	const totalPages = Math.ceil(filteredAnime.length / itemsPerPage);
@@ -208,9 +265,116 @@ const AnimeTabs = () => {
 				)}
 
 				<Grid2 container spacing={4}>
+					<Grid2
+						size={{ xs: 12, sm: 4 }}
+						sx={{ display: 'flex', gap: '1rem' }}
+					>
+						<FormControl fullWidth variant="filled">
+							<InputLabel
+								id="rating-filter-label"
+								sx={{
+									color: loading
+										? theme.palette.primary.main
+										: theme.palette.secondary.main,
+									'&:hover': {
+										color: theme.palette.secondary.main,
+									},
+									'&.Mui-focused': {
+										color: theme.palette.secondary.main,
+									},
+									'& .Mui-disabled': {
+										color: theme.palette.primary.main,
+									},
+								}}
+							>
+								Rating
+							</InputLabel>
+							<Select
+								labelId="rating-filter-label"
+								value={ratingFilter}
+								onChange={(event) =>
+									handleFilterChange(event, 'rating')
+								}
+								sx={{
+									height: '3rem',
+									border: 'solid 1px  ',
+									borderRadius: '0.25rem',
+									borderColor: loading
+										? theme.palette.primary.main
+										: theme.palette.secondary.main,
+									'& .Mui-disabled': {
+										borderColor: theme.palette.primary.main,
+									},
+								}}
+							>
+								{ratingOptions.map((option) => (
+									<MenuItem
+										key={option.value}
+										value={option.value}
+									>
+										{option.label}
+									</MenuItem>
+								))}
+							</Select>
+						</FormControl>
+					</Grid2>
+					<Grid2
+						size={{ xs: 12, sm: 4 }}
+						sx={{ display: 'flex', gap: '1rem' }}
+					>
+						<FormControl fullWidth variant="filled">
+							<InputLabel
+								id="type-filter-label"
+								sx={{
+									color: loading
+										? theme.palette.primary.main
+										: theme.palette.secondary.main,
+									'&:hover': {
+										color: theme.palette.secondary.main,
+									},
+									'&.Mui-focused': {
+										color: theme.palette.secondary.main,
+									},
+									'& .Mui-disabled': {
+										color: theme.palette.primary.main,
+									},
+								}}
+							>
+								Type
+							</InputLabel>
+							<Select
+								labelId="type-filter-label"
+								value={typeFilter}
+								onChange={(event) =>
+									handleFilterChange(event, 'type')
+								}
+								sx={{
+									height: '3rem',
+									border: 'solid 1px  ',
+									borderRadius: '0.25rem',
+									borderColor: loading
+										? theme.palette.primary.main
+										: theme.palette.secondary.main,
+									'& .Mui-disabled': {
+										borderColor: theme.palette.primary.main,
+									},
+								}}
+							>
+								{typeOptions.map((option) => (
+									<MenuItem
+										key={option.value}
+										value={option.value}
+									>
+										{option.label}
+									</MenuItem>
+								))}
+							</Select>
+						</FormControl>
+					</Grid2>
+
 					{filteredAnime.length > itemsPerPage ? (
 						<Grid2
-							size={{ xs: 12 }}
+							size={{ xs: 12, sm: 4 }}
 							sx={{ display: 'flex', justifyContent: 'flex-end' }}
 						>
 							<PagePagination
@@ -220,7 +384,12 @@ const AnimeTabs = () => {
 								onChange={handlePageChange}
 							/>
 						</Grid2>
-					) : null}
+					) : (
+						<Grid2
+							size={{ xs: 12, sm: 4 }}
+							sx={{ display: 'flex', justifyContent: 'flex-end' }}
+						></Grid2>
+					)}
 
 					{paginatedAnime.length > 0 ? (
 						paginatedAnime.map((anime) =>
