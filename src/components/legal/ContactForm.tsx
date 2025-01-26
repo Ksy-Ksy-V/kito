632c647c06a8df39c2096aeef88f93b6e65008a9
Ksy-Ksy-V <kseniia.voitikh@gmail.com>
2025-01-26 19:46:59 +0200

    Added send contact form functionslity

--- src/components/legal/ContactForm.tsx
+++ src/components/legal/ContactForm.tsx
@@ -1,22 +1,38 @@
 import { Grid2, TextField, Typography } from '@mui/material';
 import { textFieldStyles } from '../../styles/AuthStyles';
-import { useState } from 'react';
-import { useForm, ValidationError } from '@formspree/react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { useForm } from '@formspree/react';
 import theme from '../../styles/theme';
 import MainButton from '../Buttons/MainButton';
 
 const ContactForm = () => {
-	const [state, handleSubmit] = useForm(process.env.REACT_APP_FORM_ID);
+	const [state, handleSubmit] = useForm(import.meta.env.VITE_FORM_ID);
 
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [message, setMessage] = useState('');
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		message: '',
+	});
 
-	const handleSubmitForm = () => {
-		console.log(name, 'name');
-		console.log(email, 'email');
-		console.log(message, 'message');
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setFormData((prev) => ({ ...prev, [name]: value }));
 	};
+
+	const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		handleSubmit(e);
+
+		if (state.succeeded) {
+			setFormData({
+				name: '',
+				email: '',
+				message: '',
+			});
+		}
+	};
+
 	return (
 		<>
 			<Grid2 size={12}>
@@ -26,7 +42,7 @@ const ContactForm = () => {
 						textAlign: 'center',
 						margin: '1rem',
 						fontSize: {
-							xs: theme.typography.h5.fontSize,
+							xs: theme.typography.h4.fontSize,
 							sm: theme.typography.h3.fontSize,
 							md: theme.typography.h3.fontSize,
 							lg: theme.typography.h2.fontSize,
@@ -37,64 +53,87 @@ const ContactForm = () => {
 					Contact us
 				</Typography>
 			</Grid2>
-			<Grid2 size={6}>
-				<Typography
-					variant="h5"
-					sx={{
-						textAlign: 'center',
-						margin: '2rem',
-						fontSize: {
-							xs: theme.typography.body1.fontSize,
-							md: theme.typography.h5.fontSize,
-						},
-					}}
-				>
-					Have questions or need assistance?
-					<br /> We're here to help! <br />
-					Feel free to reach out to us using the form below, and we'll
-					get back to you as soon as possible.
-				</Typography>
-			</Grid2>
-			<Grid2 size={6}>
-				<form onSubmit={handleSubmitForm}>
-					<TextField
-						id="name"
-						fullWidth
-						label="Name"
-						type="name"
-						value={name}
-						name="name"
-						onChange={(event) => setName(event.target.value)}
-						sx={textFieldStyles}
-					/>
 
-					<TextField
-						id="email"
-						fullWidth
-						label="Email"
-						type="email"
-						value={email}
-						name="email"
-						onChange={(event) => setEmail(event.target.value)}
-						// helperText={validationsErrors.email}
-						sx={textFieldStyles}
-					/>
+			{state.succeeded ? (
+				<Grid2 size={12}>
+					<Typography
+						variant="h5"
+						sx={{
+							textAlign: 'center',
+							margin: '2rem',
+						}}
+					>
+						Thank you for your message!
+						<br />
+						We will get back to you as soon as possible.
+					</Typography>
+				</Grid2>
+			) : (
+				<Grid2 size={{ xs: 12, sm: 6 }}>
+					<Typography
+						variant="h5"
+						sx={{
+							textAlign: 'center',
+							margin: {
+								xs: '0rem 2rem 2rem 2rem',
+								sm: '6rem 2rem 2rem 2rem',
+							},
+							display: 'flex',
 
-					<TextField
-						id="message"
-						fullWidth
-						label="Message"
-						type="message"
-						value={message}
-						name="message"
-						multiline
-						rows={7}
-						onChange={(event) => setMessage(event.target.value)}
-						sx={textFieldStyles}
-					/>
+							alignContent: 'space-between',
+						}}
+					>
+						Have questions or need assistance?
+						<br /> We're here to help! <br />
+						Feel free to reach out to us using the form below, and
+						we'll get back to you as soon as possible.
+					</Typography>
+				</Grid2>
+			)}
 
-					<MainButton onClick={handleSubmitForm}>Send</MainButton>
-				</form>
+			<Grid2 size={{ xs: 12, sm: 6 }}>
+				{!state.succeeded && (
+					<form onSubmit={handleFormSubmit}>
+						<TextField
+							id="name"
+							fullWidth
+							label="Name"
+							type="name"
+							value={formData.name}
+							name="name"
+							onChange={handleChange}
+							sx={textFieldStyles}
+						/>
+
+						<TextField
+							id="email"
+							fullWidth
+							label="Email"
+							type="email"
+							value={formData.email}
+							name="email"
+							onChange={handleChange}
+							sx={textFieldStyles}
+						/>
+
+						<TextField
+							id="message"
+							fullWidth
+							label="Message"
+							type="message"
+							value={formData.message}
+							name="message"
+							multiline
+							rows={7}
+							onChange={handleChange}
+							sx={textFieldStyles}
+						/>
+
+						<MainButton type="submit" disabled={state.submitting}>
+							Send
+						</MainButton>
+					</form>
+				)}
 			</Grid2>
 		</>
 	);
